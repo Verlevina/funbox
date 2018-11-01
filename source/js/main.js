87b6f4c82072b8483134c7e9093ddd4c68a6f5ac
Levitina Veronika <levitinava@gmail.com>
2018-11-01 23:59:07 +0300

    решение с изменением текста по  hover через js

--- source/js/main.js
+++ source/js/main.js
@@ -1,24 +1,29 @@
 'use strict';
-var productList = new Vue({
-  el: '#productList',
-  data: {
-    products: window.data.products,
-    list: window.data.list,
-    weight: window.data.weight,
-    disabled: window.data.disable,
-    messages: window.data.messages,
+;(function () {
+  var productList = new Vue({
+    el: '#productList',
+    data: {
+      products: window.data.products,
+      list: window.data.list,
+      weight: window.data.weight,
+      disabled: window.data.disable,
+      messages: window.data.messages,
+      topMessages: [window.data.messages[0], window.data.messages[0], window.data.messages[0]],
+      checkboxes: []
+    },
 
-},
+    methods: {
+      changeTopText: function (index) {
+        if (+this.checkboxes.indexOf(index) != -1) {
+          return this.topMessages[index] = window.data.messages[1];
+        }
+        return this.topMessages[index] = window.data.messages[0];
+      },
+      changeTopTextOver: function () {
+        this.topMessages = [window.data.messages[0], window.data.messages[0], window.data.messages[0]]
+      }
+    },
 
-methods: {
-
-}
-,
-
-computed: {
-  message: function () {
-    return this.messages[0];
-  },
-}
-})
-;
+    computed: {}
+  });
+})();
